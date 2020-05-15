c99b509dfbe27df9a0e9e61e265330ff5d4a072f
Anand Graves <anandgraves@users.noreply.github.com>
2020-05-15 20:33:30 +0200

    refactor: setup homepage template for netlify cms

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,28 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
         netlify-cms-nuxt-starter-template
       </h1>
       <h2 class="subtitle">
         My marvelous Nuxt.js project
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  async asyncData({ params }) {
+    const post = await import(`~/content/homepage.md`)
+    const attr = post.attributes
+
+    const { title } = attr
+
+    return {
+      title,
+      html: post.html
+    }
   }
 }
 </script>
