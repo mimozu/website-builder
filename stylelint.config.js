d692a9d5bb0074f138b680a11166fad3d0244f8b
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-06 12:24:20 +0200

    chore: update dev dependencies and config files

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  plugins: ['stylelint-prettier'],
+  rules: {
+    'prettier/prettier': true
+  }
 }
