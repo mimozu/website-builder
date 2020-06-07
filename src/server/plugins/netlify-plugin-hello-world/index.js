79bbc661fcf854cad0dfe86eb56fc47102961078
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-07 22:26:37 +0200

    fix: try relative path

--- src/server/plugins/netlify-plugin-hello-world/index.js
+++ src/server/plugins/netlify-plugin-hello-world/index.js
@@ -5,10 +5,12 @@ module.exports = {
     console.log('Hello world from onPreBuild event!')
     try {
       const image = await jimp.read(
-        '/src/client/static/images/uploads/jump.jpg'
+        '../../../client/static/images/uploads/jump.jpg'
       )
       await image.resize(150, jimp.AUTO)
-      await image.writeAsync('/src/client/static/images/uploads/jump-150w.jpg')
+      await image.writeAsync(
+        '../../../client/static/images/uploads/jump-150w.jpg'
+      )
     } catch (error) {
       utils.build.failBuild(
         'Failure message from netlify-plugin-hello-worldddd',
