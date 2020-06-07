e53afbe9d6f97b61da486de14859989a7ee4914a
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-07 22:19:54 +0200

    fix: update path

--- src/server/plugins/netlify-plugin-hello-world/index.js
+++ src/server/plugins/netlify-plugin-hello-world/index.js
@@ -4,9 +4,9 @@ module.exports = {
   onPreBuild: async ({ utils }) => {
     console.log('Hello world from onPreBuild event!')
     try {
-      const image = await jimp.read('../client/static/images/jump.jpg')
+      const image = await jimp.read('/client/static/images/jump.jpg')
       await image.resize(150, jimp.AUTO)
-      await image.writeAsync('../client/static/images/jump-150w.jpg')
+      await image.writeAsync('/client/static/images/jump-150w.jpg')
     } catch (error) {
       utils.build.failBuild(
         'Failure message from netlify-plugin-hello-worldddd',
