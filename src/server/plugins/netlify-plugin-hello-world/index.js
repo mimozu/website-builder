d7e5d3896bb2ee8de52aff48b44de04bb049404f
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-07 21:47:39 +0200

    feat: test image resize

--- src/server/plugins/netlify-plugin-hello-world/index.js
+++ src/server/plugins/netlify-plugin-hello-world/index.js
@@ -1,5 +1,17 @@
+import jimp from 'jimp'
+
 module.exports = {
-  onPreBuild: () => {
+  onPreBuild: async ({ utils }) => {
     console.log('Hello world from onPreBuild event!')
+    try {
+      const image = await jimp.read('../client/static/images/jump.jpg')
+      await image.resize(150, jimp.AUTO)
+      await image.writeAsync('../client/static/images/jump-150w.jpg')
+    } catch (error) {
+      utils.build.failBuild(
+        'Failure message from netlify-plugin-hello-worldddd',
+        { error }
+      )
+    }
   }
 }
