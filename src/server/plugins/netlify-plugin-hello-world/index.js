1eb476b24a4fa3bb74672a55333e647a3e3a8330
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-07 22:23:46 +0200

    fix: updated path for uploaded files

--- src/server/plugins/netlify-plugin-hello-world/index.js
+++ src/server/plugins/netlify-plugin-hello-world/index.js
@@ -4,9 +4,11 @@ module.exports = {
   onPreBuild: async ({ utils }) => {
     console.log('Hello world from onPreBuild event!')
     try {
-      const image = await jimp.read('/src/client/static/images/jump.jpg')
+      const image = await jimp.read(
+        '/src/client/static/images/uploads/jump.jpg'
+      )
       await image.resize(150, jimp.AUTO)
-      await image.writeAsync('/src/client/static/images/jump-150w.jpg')
+      await image.writeAsync('/src/client/static/images/uploads/jump-150w.jpg')
     } catch (error) {
       utils.build.failBuild(
         'Failure message from netlify-plugin-hello-worldddd',
