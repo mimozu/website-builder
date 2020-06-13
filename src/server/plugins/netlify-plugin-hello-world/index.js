c08de1b4b7fa7f682925ee9069dd2db96e9b421f
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-13 13:28:28 +0200

    fix: issues with async functions

--- src/server/plugins/netlify-plugin-hello-world/index.js
+++ src/server/plugins/netlify-plugin-hello-world/index.js
@@ -30,17 +30,18 @@ module.exports = {
     try {
       const sizes = [150, 500, 900, 1200, 1900]
       const files = await getFilesFromDir(directoryPath).catch(console.error)
-      sizes.forEach(async (size) => {
-        await files.forEach(async (file) => {
-          const image = await jimp.read(`${directoryPath}/${file}`)
-          console.log(
-            'resized image',
-            `${directoryPath}/${stripExtension(file)}-${size}w.jpg`
-          )
-          await image.resize(size, jimp.AUTO)
-          await image.writeAsync(
-            `${directoryPath}/${stripExtension(file)}-${size}w.jpg`
-          )
+
+      sizes.forEach((size) => {
+        files.forEach(async (file) => {
+          try {
+            const image = await jimp.read(`${directoryPath}/${file}`)
+            await image.resize(size, jimp.AUTO)
+            await image.writeAsync(
+              `${directoryPath}/${stripExtension(file)}-${size}w.jpg`
+            )
+          } catch (error) {
+            console.error(error)
+          }
         })
       })
     } catch (error) {
