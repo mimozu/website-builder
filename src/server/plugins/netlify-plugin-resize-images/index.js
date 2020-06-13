df3e707cb0206729bf71fa8365867d00360c401c
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-13 20:26:43 +0200

    refactor: simplified path, updated error reporting

--- src/server/plugins/netlify-plugin-resize-images/index.js
+++ src/server/plugins/netlify-plugin-resize-images/index.js
@@ -3,10 +3,7 @@ const fs = require('fs')
 const jimp = require('jimp')
 const directoryPath = path.join(
   __dirname,
-  '..',
-  '..',
-  '..',
-  'client/static/images/uploads'
+  '../../../client/static/images/uploads'
 )
 
 async function getFilesFromDir(folderPath) {
@@ -43,9 +40,9 @@ module.exports = {
         }
       }
     } catch (error) {
-      utils.build.failBuild('Failure message from netlify-plugin-hello-world', {
-        error
-      })
+      return utils.build.failPlugin(
+        'netlify-plugin-resize-images: something went wrong with resizing images'
+      )
     }
   }
 }
