7f965a0811785816fa0532105ef7ab03dc395c4d
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-13 10:51:53 +0200

    refactor: resize images

--- src/server/plugins/netlify-plugin-hello-world/index.js
+++ src/server/plugins/netlify-plugin-hello-world/index.js
@@ -1,26 +1,32 @@
 const path = require('path')
 const fs = require('fs')
 const jimp = require('jimp')
+const directoryPath = path.join(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  'client/static/images/uploads'
+)
+
+async function getFilesFromDir(folderPath) {
+  try {
+    return await fs.promises.readdir(folderPath)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 module.exports = {
   onPreBuild: async ({ utils }) => {
-    const directoryPath = path.join(
-      __dirname,
-      '..',
-      '..',
-      '..',
-      'client/static/images/uploads'
-    )
-    console.log('Hello world from onPreBuild event!. Path is ', directoryPath)
-    const files = await fs.promises.readdir(directoryPath)
-    for (const file of files) {
-      console.log(file)
-    }
-
     try {
-      const image = await jimp.read('src/client/static/images/uploads/jump.jpg')
-      await image.resize(150, jimp.AUTO)
-      await image.writeAsync('src/client/static/images/uploads/jump-150w.jpg')
+      const files = getFilesFromDir(directoryPath).catch(console.error)
+      files.forEach(async (file) => {
+        console.log(file)
+        const image = await jimp.read(`${directoryPath}/${file}`)
+        await image.resize(150, jimp.AUTO)
+        await image.writeAsync(`${directoryPath}/${file}-150w.jpg`)
+      })
     } catch (error) {
       utils.build.failBuild(
         'Failure message from netlify-plugin-hello-worldddd',
