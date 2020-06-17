3a1a40b1241df68f5cfe7c46b66d9e1673335843
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-17 16:31:13 +0200

    refactor(index.js): decrease webp quality to 40

--- src/server/plugins/netlify-plugin-resize-images/index.js
+++ src/server/plugins/netlify-plugin-resize-images/index.js
@@ -47,7 +47,7 @@ module.exports = {
             }w.jpg`
           )
           await sharp(`${directoryPath}/${files[indexFiles]}`)
-            .webp({ quality: 45 })
+            .webp({ quality: 40 })
             .toFile(
               `${directoryPath}/${stripExtension(files[indexFiles])}-${
                 sizes[indexSizes]
