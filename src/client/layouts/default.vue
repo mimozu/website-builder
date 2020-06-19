24198384394c9812f33231f1ff4be9068b57f650
Anand Graves <anandgraves@users.noreply.github.com>
2020-06-19 22:52:54 +0200

    feat: add favicons and declarations

--- src/client/layouts/default.vue
+++ src/client/layouts/default.vue
@@ -4,6 +4,53 @@
   </div>
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      meta: [
+        {
+          name: 'msapplication-TileColor',
+          content: '#da532c'
+        },
+        {
+          name: 'theme-color',
+          content: '#ffffff'
+        }
+      ],
+      link: [
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png'
+        },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest'
+        },
+        {
+          rel: 'mask-icon',
+          href: '/safari-pinned-tab.svg',
+          color: '#5bbad5'
+        }
+      ]
+    }
+  }
+}
+</script>
+
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
