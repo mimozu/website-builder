39f5a9a62d219aa8e7fa07a4097c787583232b5c
Anand Graves <anandgraves@users.noreply.github.com>
2020-07-07 13:19:15 +0200

    feat: add nuxt/pwa and a favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,9 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/stylelint-module',
+    // Doc: https://github.com/nuxt-community/pwa-module
+    '@nuxtjs/pwa'
   ],
   /*
    ** Nuxt.js modules
